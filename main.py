a0e4a2acd6c5bc12fcbee3f9f2d0ce88b38956bd
KiWlidee <mihailgasnikov35@gmail.com>
2025-09-27 15:35:53 +0300

    I have configured the sending of messages and the delay between them. Added another def. There are 3 commands available so far. Still the code in the same file.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import requests
-
 from datetime import datetime
 
 from selenium import webdriver
@@ -13,8 +11,6 @@ from bs4 import BeautifulSoup
 browser = webdriver.Firefox()
 browser.get("https://funpay.com/account/login")
 
-
-
 # Вход на funpay
 
 elem = browser.find_element(By.NAME, "login")
@@ -24,24 +20,51 @@ elem.send_keys("AndroidDinamo")
 input()
 
 
-# Заходим в чаты
+def first_file_w():
+    global content2
+    soup = parcer_chats()
+    browser.refresh()
+    sleep(1.5)
+    all_chats = soup.find("div", class_="contact-list custom-scroll")
+    with open("C:\\vscodepj\\funpay_bot\\html_file1.txt", "w", encoding="utf-8") as f:
+        f.write(str(all_chats.text))
+
+    content2 = all_chats.text
+    content2 = str(content2).split()
+
+def chat_link_logs(word):
+    chat_find = all_chats.find("a").get("href")
+    if word == "/people":
+        with open("C:\\vscodepj\\funpay_bot\\logs.txt", "a", encoding="utf-8") as f:
+            f.write(f"{datetime.now()} - Позвали человека. Ссылка на чат - {chat_find}\n")  #  Записываем логи
+    elif word == "/new_message":
+        with open("C:\\vscodepj\\funpay_bot\\logs.txt", "a", encoding="utf-8") as f:
+            f.write(f"{datetime.now()} - Пришло новое сообщение. Ссылка на чат - {chat_find}\n")  #  Записываем логи
+    elif word == "/rent":
+        with open("C:\\vscodepj\\funpay_bot\\logs.txt", "a", encoding="utf-8") as f:
+            f.write(f"{datetime.now()} - Купили аренду. Ссылка на чат - {chat_find}\n")  #  Записываем логи
+    return chat_find
+
+
 def parcer_chats():  #  Парсим html код со страницы чатов
-    sleep(2)
+    sleep(1.5)
     browser.get("https://funpay.com/chat/")
     full_html = browser.page_source
     soup = BeautifulSoup(full_html, "lxml")
     return soup
+
+# Заходим в чаты
 browser.get("https://funpay.com/chat/")
 soup = parcer_chats()
 all_chats = soup.find("div", class_="contact-list custom-scroll")  #  Снова парсим отдельно чаты
 
 with open("C:\\vscodepj\\funpay_bot\\html_file1.txt", "w", encoding="utf-8") as f:
     f.write(str(all_chats.text))
-sleep(2)  #  Записываем в первый файл наши только что скрепенные данные
+sleep(1.5)  #  Записываем в первый файл наши только что скрепенные данные
 while True:
-    sleep(2)
+    sleep(1.5)
     soup = parcer_chats()
-    sleep(1)
+    sleep(1.5)
     all_chats = soup.find("div", class_="contact-list custom-scroll")
     with open("C:\\vscodepj\\funpay_bot\\html_file2.txt", "w", encoding="utf-8") as f:
         f.write(str(all_chats.text))
@@ -53,19 +76,14 @@ while True:
         else:
             soup = parcer_chats()
             browser.refresh()
-            sleep(2)
-            with open("C:\\vscodepj\\funpay_bot\\logs.txt", "a", encoding="utf-8") as f:
-                f.write(f"{datetime.now()} - Пришло новое сообщение. Ссылка на чат - {chat_find}\n")  #  Записываем логи
-            
-            with open("C:\\vscodepj\\funpay_bot\\html_file1.txt", "w", encoding="utf-8") as f:
-                f.write(str(all_chats.text))
+            sleep(1.5)
 
-            content2 = all_chats.text
-            content2 = str(content2).split()
-
+            first_file_w()
             if content2[1] == "/people":
-                chat_find = all_chats.find("a").get("href")
+                chat_find = chat_link_logs("/people")
+
                 browser.get(chat_find)
+                
                 elem = browser.find_element(By.NAME, "content")
                 elem.send_keys("Понял! Сейчас отправлю уведомление продавцу, и если он не спит, то сразу ответит. " \
                 "Больше ничего писать не нужно.")
@@ -75,71 +93,21 @@ while True:
                 soup = parcer_chats()
                 browser.refresh()
                 #
-                sleep(2)
-                all_chats = soup.find("div", class_="contact-list custom-scroll")
-                with open("C:\\vscodepj\\funpay_bot\\html_file1.txt", "w", encoding="utf-8") as f:
-                    f.write(str(all_chats.text))
-
-                content2 = all_chats.text
-                content2 = str(content2).split()
+                first_file_w()
                 #
-            elif content2[1] == '/error':
-                chat_find = all_chats.find("a").get("href")
-                browser.get(chat_find)
-                elem = browser.find_element(By.NAME, "content")
-                elem.send_keys("""Вот самые распрастраненные ошибки:
-Ошибка 0x8007003B
-
-Пробуйте:
-1) Включить впн
-2) Перезаходить в microsoft store, до тех пор, пока не исчезнет ошибка
-3) Перезагрузить роутер или зайти в cmd от имени администратора и эту команду ввести ipconfig /flushdns, потом перезагрузить пк.
-
-0x80190001
-(Обычно помогает просто сбросить кэш(2) и включил впн)
-
-1. Проверьте интернет-соединение: Убедитесь, что ваше интернет-соединение стабильно и работает правильно. 
-2. Сбросьте кэш Microsoft Store: Нажмите Win + R, введите wsreset.exe и нажмите Enter. 
-3. Проверьте настройки сети: Убедитесь, что настройки DNS-сервера установлены правильно или попробуйте сбросить сетевые настройки. 
-4. Перезапустите службу обновлений Windows: В поиске введите "Службы" и найдите службу "Центр обновления Windows". Перезапустите ее. 
-5. Проверьте настройки брандмауэра и антивируса: Убедитесь, что они не блокируют доступ к приложениям Microsoft. 
-6. Перезагрузите компьютер: Простая перезагрузка может решить временные проблемы. 
-7. Проверьте настройки времени и даты: Неправильные настройки времени и даты могут привести к проблемам с подключением к серверам Microsoft.
-Ссылка на решение большенства проблем и ошибок - https://teletype.in/@shiski/vWbPBDa6l1m""")
-                elem.send_keys(Keys.RETURN)
-
-                with open("C:\\vscodepj\\funpay_bot\\logs.txt", "a", encoding="utf-8") as f:
-                    f.write(f"{datetime.now()} - Xbox Error. Ссылка на чат - {chat_find}\n")  #  Записываем логи
-
-                sleep(1)
-                all_chats = soup.find("div", class_="contact-list custom-scroll")
-                with open("C:\\vscodepj\\funpay_bot\\html_file1.txt", "w", encoding="utf-8") as f:
-                    f.write(str(all_chats.text))
-
-                content2 = all_chats.text
-                content2 = str(content2).split()
-                sleep(1)
+            elif content2[1] == "/rent":
+                chat_find = chat_link_logs("/rent")
             else:
-                chat_find = all_chats.find("a").get("href")
+                chat_find = chat_link_logs("/new_message")
+
                 browser.get(chat_find)
-                sleep(2.5)
+                sleep(1.5)
                 elem = browser.find_element(By.NAME, "content")
                 elem.send_keys("""Привет. Я - бот созданный KiWlide и хочу помочь тебе, мой дорогой покупатель.
     Вот список команда, для управления:
     Позвать человека - /people
-    Если ты купил у меня подписку xbox и у тебя появились ошибки - /error
-    Если у тебя нет вопросов, то и писать ничего не нужно.""")
-                with open("C:\\vscodepj\\funpay_bot\\logs.txt", "a", encoding="utf-8") as f:
-                    f.write(f"{datetime.now()} - Стартовое сообщение. Ссылка на чат - {chat_find}\n")  #  Записываем логи
-                elem.send_keys(Keys.RETURN)
-                #
-                soup = parcer_chats()
-                browser.refresh()
-                sleep(2)
-                all_chats = soup.find("div", class_="contact-list custom-scroll")
-                with open("C:\\vscodepj\\funpay_bot\\html_file1.txt", "w", encoding="utf-8") as f:
-                    f.write(str(all_chats.text))
+    Вы купили у меня аренду аккаунта? - /rent
+    Если у Вас нет вопросов, то и писать ничего не нужно. Не стоит мне спамить, быстрее я не отвечу.""")
 
-                content2 = all_chats.text
-                content2 = str(content2).split()
-                #
+                elem.send_keys(Keys.RETURN)
+                first_file_w()
